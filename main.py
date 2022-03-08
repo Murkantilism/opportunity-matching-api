1ddf9f15007ffcfc35ff82eaede02eea5e41d90a
Deniz S. Ozkaynak <deniz.ozkaynak@gmail.com>
2022-03-08 18:37:02 -0500

    Implement a crude matching algo

--- main.py
+++ main.py
@@ -5,27 +5,34 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
+matches = []
 
-def process_files():
-    users_file = open('users.json')
-    users = json.load(users_file)
-    opportunities_file = open('opportunities.json')
-    opportunities = json.load(opportunities_file)
+users_file = open('users.json')
+users = json.load(users_file)
+opportunities_file = open('opportunities.json')
+opportunities = json.load(opportunities_file)
 
+def naive_matching(users):
+    for opp in opportunities:
+        if (opp['roles'] and len(opp['roles']) > 0):
+            for role in opp['roles']:
+                for user in users:
+                    if (user and user['interested_in'] and len(user['interested_in']) > 0):
+                        if (role in user['interested_in']):
+                            matches.append({ 'confidence': 100, 'role_matched': role, 'candidate_matched': user, 'organization_matched': { 'organization': opp['organization'], 'email': opp['email'] } })
     
-    # TODO: loop over/serialize files to perform matching
-
     users_file.close()
     opportunities_file.close()
 
+naive_matching(users)
+
 class Matches(Resource):
-    def get(self):
-        return [{
-            'foo': 'bar',
-        }]
+    def get(self, limit_to_top_results = 10):
+        print(matches)
+        return matches
 
 api.add_resource(Matches, '/matches');
 
 if __name__ == '__main__':
-    process_files()
+    
     app.run(debug=True)
