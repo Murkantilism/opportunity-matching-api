da0383e436e847d4585a240e9a8a03322962be9d
Deniz S. Ozkaynak <deniz.ozkaynak@gmail.com>
2022-03-08 17:51:00 -0500

    Sketching file ingestion

---
+++ main.py
@@ -0,0 +1,13 @@
++import json
+
+users_file = open('.\users.json')
+users = json.load(users_file)
+opportunities_file = open('.\opportunities.json')
+opportunities = json.load(opportunities_file)
+
+def get_matches ():
+    matches = []
+	return
+    
+users_file.close()
+opportunities_file.close()
