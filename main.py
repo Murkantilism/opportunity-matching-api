6de25a9bcd3a70567dd54ff3703217975279cef0
Deniz S. Ozkaynak <deniz.ozkaynak@gmail.com>
2022-03-08 17:51:14 -0500

    Setup flask app

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
-+import json
+import json
+from flask import Flask
+from flask_restful import Resource, Api
 
-users_file = open('.\users.json')
-users = json.load(users_file)
-opportunities_file = open('.\opportunities.json')
-opportunities = json.load(opportunities_file)
+app = Flask(__name__)
+api = Api(app)
 
-def get_matches ():
-    matches = []
-	return
+
+def process_files():
+    users_file = open('users.json')
+    users = json.load(users_file)
+    opportunities_file = open('opportunities.json')
+    opportunities = json.load(opportunities_file)
+
     
-users_file.close()
-opportunities_file.close()
+    # TODO: loop over/serialize files to perform matching
+
+    users_file.close()
+    opportunities_file.close()
+
+class Matches(Resource):
+    def get(self):
+        return [{
+            'foo': 'bar',
+        }]
+
+api.add_resource(Matches, '/matches');
+
+
+
+if __name__ == '__main__':
+    process_files()
+    app.run(debug=True)
